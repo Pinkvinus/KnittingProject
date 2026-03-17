4de87521cd3868387625d8c54442f625a5900013
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2026-03-17 11:22:46 +0100

    added tool menu

---
+++ knit-frontend/src/app/layout/ProjectEditor/sidebar/toolmenu/toolmenu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolmenu',
+  imports: [],
+  templateUrl: './toolmenu.html',
+  styleUrl: './toolmenu.css',
+})
+export class Toolmenu {
+
+}
