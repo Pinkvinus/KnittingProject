2fe8fc0c8fb03d997d0d3a0966c70ac2bc87e857
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2026-03-23 10:15:11 +0100

    Added shape menu

--- knit-frontend/src/app/layout/ProjectEditor/sidebar/toolmenu/toolmenu.ts
+++ knit-frontend/src/app/layout/ProjectEditor/sidebar/toolmenu/toolmenu.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Yarnmenu } from './yarnmenu/yarnmenu'; 
+import { Shapemenu } from './shapemenu/shapemenu';
 
 @Component({
   selector: 'app-toolmenu',
-  imports: [CommonModule, Yarnmenu],
+  imports: [CommonModule, Yarnmenu, Shapemenu],
   templateUrl: './toolmenu.html',
   styleUrl: './toolmenu.css',
 })
